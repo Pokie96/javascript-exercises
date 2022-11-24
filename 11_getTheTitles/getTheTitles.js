bc545265550d40f87617517554d6c6e8f677c011
Adam Christie <19adamchristie96@gmail.com>
2022-11-24 21:50:47 +0000

    Create function which takes an array and returns an array containing only the title values of the objects in the initial array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titlesArray = array.map(function(item){
+        return item.title;
+    })
+    return titlesArray;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
