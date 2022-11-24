8ca06fc2f0ebc1490fd5d65e5df915c02fbbab4f
Adam Christie <19adamchristie96@gmail.com>
2022-11-24 21:03:57 +0000

    Create function that finds the nth fibonacci number in a sequence returning the number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibArray = [1, 1];
+    for(let i = 1; i <= n; i++){
+        let thisNum = fibArray[i-1] + fibArray[i];
+        fibArray.push(thisNum);
+    }
+    return fibArray[n-1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
