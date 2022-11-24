4f35ff09ec4c6e5692cb1b0993681e88192aa9dc
Adam Christie <19adamchristie96@gmail.com>
2022-11-24 04:04:58 +0000

    Complete and test function to return true or false whether a word or sentence is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let originalWordArray = word.split("");
+    let originalWord = originalWordArray.filter(
+        char => char >= 'a' && char <= 'z' 
+        || char >= 'A' && char <= 'Z').join('');
+    originalWord = originalWord.toLowerCase()
+    let newWordArray = originalWord.split("");
+    newWordArray.reverse();
+    let newWord = newWordArray.join("");
+    console.log(originalWord)
+    console.log(newWord)
+    return originalWord === newWord ? true : false;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
