9d0e55df03bd0c4db3f4002fd5a22bfc2c6950a9
Adam Christie <19adamchristie96@gmail.com>
2022-11-25 00:33:33 +0000

    Create function which receives an array of people, calculates their years lived and returns the oldest of the people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b){
+        if(!b.yearOfDeath){
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0];
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+
+  console.log(findTheOldest(people));
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
